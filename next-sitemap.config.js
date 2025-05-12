25bcc7ec6d867d763866019290054afa699ce37f
gribelUS <tiagobreunig1@gmail.com>
2025-05-12 14:12:57 -0400

    Different approach

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://tiagobreunig.com',
+    generateRobotsTxt: true, // (optional) generates robots.txt file
+  };
+  
